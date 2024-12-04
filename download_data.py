f89f7419d6b83a2a66671a2146535ddbd9e8426a
Caroline-kahare <kaharec@student.ubc.ca>
2024-12-04 12:00:56 -0800

    updated comments

--- download_data.py
+++ download_data.py
@@ -10,9 +10,14 @@ def combine_files(output_file):
     ]
     columns = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 
                'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'label']
-
+    
+    #read file into dataframes
     dataframes = [pd.read_csv(fp, index_col=False, names=columns) for fp in file_paths]
+    
+    #put all the dataframes into one
     combined_df = pd.concat(dataframes, ignore_index=True)
+    
+    #save combined df
     combined_df.to_csv(output_file, index=False)
     print(f"Combined data saved to {output_file}")
 
